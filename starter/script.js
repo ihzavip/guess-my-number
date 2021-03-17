f029f452d5611b39cc5e3dbd2356d82c3ac701c9
ihzavip <ihzavip@gmail.com>
2021-03-18 03:11:00 +0700

    kerjain aja jangan banyak tingkah

---
+++ starter/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//console.log(document.querySelector('.message').textContent);
+//document.querySelector('.message').textContent = 'Kemem Hangat';
+
+//// It's input value
+//document.querySelector('.guess').value = 23;
+//console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
